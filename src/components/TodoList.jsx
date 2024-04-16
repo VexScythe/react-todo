ffeaf3fa06979d0cf0fad3ea3c976a133183adc3
VexScythe <davide.ludovici@yahoo.it>
2024-04-16 16:57:14 +0200

    fix: no item color

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import { TodoItem } from './TodoItem';
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul>
-      {todos.length === 0 && <p className="text-emerald-400 m-6">No Todos</p>}
+      {todos.length === 0 && <p className="text-zinc-900 m-6">No Todos</p>}
       {todos.map((todo) => {
         return <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />;
       })}
